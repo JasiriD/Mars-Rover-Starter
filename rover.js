3bbc82a1226bbccd2d25ad469fb33edffed79a88
unknown <jasiridickson@gmail.com>
2023-11-07 14:33:05 -0600

    Graded assignment 3

--- rover.js
+++ rover.js
@@ -17,7 +17,7 @@ class Rover {
    receiveMessage(message){
       
       let output = {
-         messageName: message.name, 
+         message: message.name, 
          messageCommand: message.commands,
          results:
          [
@@ -25,35 +25,65 @@ class Rover {
          ]
       };
 
-      //For loop that loops though the array of commands and dynamically updated the results array within the output object
-      for(let i = 0; i < message.commands.length; i++){
+      //If statement that checks wheter the commands array is empty or not
+      if(message.commands !== undefined){
 
-         //If statement that checks if the current entry in the array is "MODE_CHANGE"
-         if(message.commands[i].commandType === "MODE_CHANGE"){
-            //Checks if the mode is to be changed to "LOW_POWER" and changes it if so
-            if(message.commands[i].value === "LOW_POWER"){
-               output.results.push({completed: true})
-               this.mode = "LOW_POWER";
+         //For loop that loops though the array of commands and dynamically updated the results array within the output object
+         for(let i = 0; i < message.commands.length; i++){
+
+            //If statement that checks if the current entry in the array is "MODE_CHANGE"
+            if(message.commands[i].commandType === "MODE_CHANGE"){
+
+               //Checks if the mode is to be changed to "LOW_POWER" and changes it if so
+               if(message.commands[i].value === "LOW_POWER"){
+                  output.results.push({completed: true})
+                  this.mode = "LOW_POWER";
+               }
+               //Does the same for "NORMAL"
+               else if(message.commands[i].value === "NORMAL"){
+                  output.results.push({completed: true})
+                  this.mode = "NORMAL";
+               }
+               //If it's neither set to normal
+               else{
+                  this.mode = "NORMAL";
+               }
             }
-            //Does the same for "NORMAL"
-            else if(message.commands[i].value === "NORMAL"){
-               output.results.push({completed: true})
-               this.mode = "NORMAL";
+
+            //If statement that checks if the current entry in the array is "STATUS_CHECK"
+            if(message.commands[i].commandType === "STATUS_CHECK"){
+               output.results.push({
+                  completed: true,
+                  roverStatus: {position: this.position, mode: this.mode, generatorWatts: this.generatorWatts,
+                  }})
+               console.log("Works");
             }
-            //If it's neither set to normal
-            else{
-               this.mode = "NORMAL";
+
+            //If statement that checks if the current entry in the array is "MOVE"
+            if(message.commands[i].commandType === "MOVE"){
+
+               //Checks if rover is in low power mode
+               if(this.mode !== "LOW_POWER"){
+                  output.results.push({
+                     completed: true,
+                  })
+
+                  //Sets rover's position to position given in command
+                  this.position = message.commands[i].value
+               }
+
+               //If rover is in low power mode
+               else{
+                  output.results.push({
+                     completed: false,
+                  })
+               }
             }
+
+
          }
-
-         //If statement that checks if the current entry in the array is "STATUS_CHECK"
-         if(message.commands[i].commandType === "STATUS_CHECK"){
-            output.results.push({completed: true})
-            
-         }
-
-
       }
+
       /*
       roverStatus = {
          position: this.position,
@@ -71,12 +101,14 @@ class Rover {
 
 }
 
-let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-let message = new Message('Test message with two commands', commands);
-let rover = new Rover(98382);    // Passes 98382 as the rover's position.
-let response = rover.receiveMessage(message);
+// let commands = [new Command('STATUS_CHECK')];
+// let message = new Message('Test message with two commands', commands);
+// let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+// let response = rover.receiveMessage(message);
 
-console.log(response.results);
-console.log(rover.mode)
+// //console.log(response.results[1].roverStatus.position);
+// console.log(response.messageName)
+// console.log(response.messageCommand[0])
+// console.log(response.results[0].roverStatus);
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -29,6 +29,7 @@ describe("Rover class", function() {
   //Test 8: Makes sure the recieveMessage function returns the name of the given message
 
   test("Response returned by recieveMessage contains name of given message", function(){
+
     //Creates new message object with the name "name"
     let message = new Message("Name");
 
@@ -36,14 +37,15 @@ describe("Rover class", function() {
     let rover = new Rover();
 
     //Storing result of function into a temp object
-    let tempResult = rover.recieveMessage(message);
+    let output = rover.receiveMessage(message);
     
-    expect(tempResult.messageName).toEqual("Name"); 
+    expect(output.message).toEqual("Name");
   })
 
   //Test 9: Makes sure recieveMessage contains 2 results if 2 commands are sent in the message
 
   test("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+
     //Creates new commands array
     let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
 
@@ -53,7 +55,7 @@ describe("Rover class", function() {
     let rover = new Rover();
 
     //Assigns function output to a variable
-    let output = rover.recieveMessage(message);
+    let output = rover.receiveMessage(message);
 
     let arrayLength = output.messageCommand.length;
 
@@ -71,18 +73,24 @@ test("Responds correctly to the status check command", function(){
 
   let message = new Message("Test STATUS_CHECK command", commands);
 
+  //Sets position of rover object to 100
   let rover = new Rover(100)
 
-  let output = rover.recieveMessage(message);
+  let output = rover.receiveMessage(message);
 
-    expect(output.roverStatus.mode).toEqual('NORMAL');
-    expect(output.roverStatus.position).toEqual(100);
-    expect(output.roverStatus.generatorWatts).toEqual(110);
+    //Checks if completed is set to true
+    expect(output.results[0].completed).toEqual(true);
+
+    //Checks if rthe results array contains the roverstatus object and the status of the rover
+    expect(output.results[0].roverStatus.position).toEqual(100);
+    expect(output.results[0].roverStatus.generatorWatts).toEqual(110);
+    expect(output.results[0].roverStatus.mode).toEqual("NORMAL");
 })
 
 //Test 11: Rover correctly changes mode when MODE_CHANGE command is submitted
 
 test("Responds correctly to the mode change command", function(){
+
   //Commands array made to change mode of rover
   let commands = [new Command('MODE_CHANGE', 'LOW_POWER')];
 
@@ -90,10 +98,67 @@ test("Responds correctly to the mode change command", function(){
 
   let rover = new Rover();
 
-  let output = rover.recieveMessage(message);
+  let output = rover.receiveMessage(message);
 
-  expect(output.completed )
+  //Checks if completed is set to true
+  expect(output.results[0].completed).toEqual(true);
 
+  //Checks if the status of the rover (mode in the rover object) is changed to "LOW_POWER"
+  expect(rover.mode).toEqual("LOW_POWER");
+
+})
+
+//Test 12: Makes sure rover cannot move while in "LOW_POWER" mode
+
+test("Responds with a false completed value when attempting to move in LOW_POWER mode", function(){
+
+  //Sets rover to low power mode
+  let commands = [new Command('MODE_CHANGE', 'LOW_POWER')];
+
+  let message = new Message("Testing moving in low power mode", commands);
+
+  //Sets rover's position to 100
+  let rover = new Rover(100);
+
+  let output = rover.receiveMessage(message)
+
+  //Checking if rover is properly set to low power mode before moving forward
+  expect(rover.mode).toEqual("LOW_POWER"); 
+
+  //Setting commands array to move command
+  commands = [new Command('MOVE', 123456)];
+
+  //Giving message variable the new commands array
+  message = new Message("Testing moving in low power mode", commands);
+
+  //Running function again in an attempt to move
+  output = rover.receiveMessage(message)
+
+  //Checks if completed is set to false
+  expect(output.results[0].completed).toEqual(false);
+
+  //Makes sure rover's position is unchanged
+  expect(rover.position).toEqual(100);
+
+
+})
+
+//Test 13: Makes sure MOVE command works as intended
+
+test("responds with the position for the move command", function(){
+
+    //Move command
+    let commands = [new Command('MOVE', 123456)];
+
+    let message = new Message("Testing MOVE command", commands);
   
+    //Sets rover's position to 100
+    let rover = new Rover(100);
+  
+    //Running move command
+    let output = rover.receiveMessage(message)
+
+    //Tests rover's new position
+    expect(rover.position).toEqual(123456);
 
 })
